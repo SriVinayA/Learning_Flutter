304c1fa312f35828d3d8dc1201e425bd1aa7b03c
SriVinayA <srivinay.appari@gmail.com>
2019-05-25 22:06:45 +0530

    minor fixes

--- hello_world/lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main(){
-  runApp(new MaterialApp(home: Text("Hello World")));
-}
---
+++ scaffold_app_2/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "My Application",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Home Page"),
+        ),
+        body: new Center(
+          child: new Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
