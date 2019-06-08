7d20f18676384f53872be3ec330bf5039cef5eaa
SriVinayA <srivinay.appari@gmail.com>
2019-06-08 16:13:34 +0530

    searching

--- list_view/lib/main.dart
+++ list_view/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'searching/binary.dart';
+import 'searching/interpolation.dart';
+import 'searching/jump.dart';
+import 'searching/linear.dart';
 import 'sorting/bubble.dart';
 import 'sorting/insertion.dart';
 import 'sorting/merge.dart';
@@ -15,11 +19,17 @@ class MyApp extends StatelessWidget {
         title: 'List_view',
         home: HomePage(),
         routes: <String, WidgetBuilder>{
+          //Sorting
           "/BubbleSort": (BuildContext context) => BubbleSort("Bubble Sort"),
           "/MergeSort": (BuildContext context) => MergeSort("Merge Sort"),
           "/InsertionSort": (BuildContext context) => InsertionSort("Insertion Sort"),
           "/ShellSort": (BuildContext context) => ShellSort("Shell Sort"),
           "/SelectionSort": (BuildContext context) => SelectionSort("Selection Sort"),
+          //Searching
+          "/LinearSearch": (BuildContext context) => LinearSearch("Linear Search"),
+          "/BinarySearch": (BuildContext context) => BinarySearch("Binary Search"),
+          "/JumpSearch": (BuildContext context) => JumpSearch("Jump Search"),
+          "/InterpolationSearch": (BuildContext context) => InterpolationSearch("Interpolation Search"),
         });
   }
 }
@@ -86,6 +96,43 @@ class _HomePageState extends State<HomePage> {
               height: 30,
             ),
             title: Text("Selection Sort"),
+          ),
+          Divider(),
+          ListTile(
+            onTap: () => Navigator.of(context).pushNamed("/LinearSearch"),
+            leading: Image.network(
+              "http://www.pure-informatique.com/wp-content/uploads/2017/05/open-book.png",
+              width: 30,
+              height: 30,
+            ),
+            title: Text("Linear Search"),
+          ),
+          ListTile(
+            onTap: () => Navigator.of(context).pushNamed("/BinarySearch"),
+            leading: Image.network(
+              "http://www.pure-informatique.com/wp-content/uploads/2017/05/open-book.png",
+              width: 30,
+              height: 30,
+            ),
+            title: Text("Binary Search"),
+          ),
+          ListTile(
+            onTap: () => Navigator.of(context).pushNamed("/JumpSearch"),
+            leading: Image.network(
+              "http://www.pure-informatique.com/wp-content/uploads/2017/05/open-book.png",
+              width: 30,
+              height: 30,
+            ),
+            title: Text("Jump Search"),
+          ),
+          ListTile(
+            onTap: () => Navigator.of(context).pushNamed("/InterpolationSearch"),
+            leading: Image.network(
+              "http://www.pure-informatique.com/wp-content/uploads/2017/05/open-book.png",
+              width: 30,
+              height: 30,
+            ),
+            title: Text("Interpolation Search"),
           ),
         ],
       ),
---
+++ list_view/lib/mainlist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mainlist extends StatefulWidget {
+  @override
+  _MainlistState createState() => _MainlistState();
+}
+
+class _MainlistState extends State<Mainlist> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
